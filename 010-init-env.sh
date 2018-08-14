4e428c2bc2f2da941811459cd28edd7759fbc4cf
JeongHyeLee <wjdgp125@naver.com>
2018-08-14 05:15:44 +0000

    2018.08.14_edit

--- 010-init-env.sh
+++ 010-init-env.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-(( EUID )) && echo You need to be root. && exit 1
 set -ex
-
-export LC_ALL="en_US.UTF-8"
-export PATH=$PATH:/usr/local/bin
-
-# To prevent locale error during the installation. Added locale settings.
-echo LANG=en_US.utf-8 >> /etc/environment
-echo LC_ALL=en_US.utf-8 >> /etc/environment
-echo export LANG=en_US.utf-8 >> ~/.bashrc
-echo export LC_ALL=en_US.utf-8 >> ~/.bashrc
-
-
-EXIP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $7}')
-echo $EXIP 'taco-aio' >> /etc/hosts
 
 OS_DISTRO=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
 if [ $OS_DISTRO == Red ]; then
@@ -36,18 +22,16 @@ elif [ $OS_DISTRO == CentOS ]; then
     # TODO Don't leave firewall disabled permanently, use only for TACO-AIO
     systemctl disable firewalld
 elif [ $OS_DISTRO == Ubuntu ]; then
-    apt-get update
-    apt-get -y upgrade
-    apt install -y python python-pip
-    apt install -y ceph-common git jq nmap bridge-utils ipcalc
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt install -y python python-pip
+    sudo apt install -y ceph-common git jq nmap bridge-utils ipcalc
 else
     echo "This Linux Distribution is NOT supported"
 fi
-pip install --upgrade pip==9.0.3
-pip install 'pyOpenSSL==16.2.0'
-pip install 'python-openstackclient'
+sudo pip install --upgrade pip==9.0.3
+sudo pip install 'pyOpenSSL==16.2.0'
+sudo pip install 'python-openstackclient'
 
-swapoff -a
-sed -i '/swap/s/^/#/g' /etc/fstab
-modprobe rbd
-ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
+sudo sed -i '/swap/s/^/#/g' /etc/fstab
+sudo modprobe rbd
--- 020-install-k8s.sh
+++ 020-install-k8s.sh
@@ -1,79 +1,105 @@
 #!/bin/bash
-(( EUID )) && echo You need to be root. && exit 1
 set -ex
 export PATH=$PATH:/usr/local/bin
 cd ~/
+
 mkdir -p ~/apps
 TACO_KUBESPRAY_DIR=~/apps/taco-kubespray
 if [ -d $TACO_KUBESPRAY_DIR ]; then
   rm -rf $TACO_KUBESPRAY_DIR
 fi
+
 UPSTREAM_KUBESPRAY_DIR=~/apps/upstream-kubespray
 if [ -d $UPSTREAM_KUBESPRAY_DIR ]; then
   rm -rf $UPSTREAM_KUBESPRAY_DIR
 fi
+
 KUBESPRAY_DIR=~/apps/kubespray
 if [ -d $KUBESPRAY_DIR ]; then
   rm -rf $KUBESPRAY_DIR
 fi
+
 CACHE_FILE=/tmp/taco-aio
 if [ -f $CACHE_FILE ]; then
   rm -f $CACHE_FILE
 fi
+
 cd ~/apps
 git clone https://github.com/kubernetes-incubator/kubespray.git upstream-kubespray && cd upstream-kubespray
-git checkout -b v2.4.0 tags/v2.4.0
 pip install -r requirements.txt
 
-cd ~/apps
-git clone https://github.com/sktelecom-oslab/taco-kubespray.git && cd taco-kubespray
-git checkout -b v2.4.0 tags/v2.4.0
+HOST=~/apps/upstream-kubespray/inventory/sample/hosts.ini
 
-cd ~/apps
-cp -R upstream-kubespray kubespray && cp -R taco-kubespray/* kubespray/. && cd kubespray
-echo """taco-aio ansible_connection=local local_release_dir={{ansible_env.HOME}}/releases 
-[kube-master]
-taco-aio
-[etcd]
-taco-aio
-[kube-node]
-taco-aio
-[k8s-cluster:children]
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
+
+echo "[enter the hostname that you want to use as master and worker node and ip address respectively]"
+read -p "****how many \"masters\" : " number
+for i in $(seq $number); do
+   read  -p "#${i} input master node's <hostname> <ip-address> :" hostname ip_address
+   master_array[${i}-1]=$hostname
+   echo $hostname ip=$ip_address>>$HOST
+   ssh-copy-id $ip_address
+done
+
+read -p "****how many \"workers\" : " number
+for i in $(seq $number); do
+   read  -p "#${i} input worker node's <hostname> <ip-address> :" hostname ip_address
+   worker_array[${i}-1]=$hostname
+   echo $hostname ip=$ip_address>>$HOST
+   ssh-copy-id $ip_address
+done
+
+# if the information is in file
+# not yet
+
+echo "[kube-master]">>$HOST
+for arr_item in ${master_array[*]}
+do
+  echo $arr_item >>$HOST
+done
+
+echo "[etcd]">>$HOST
+for arr_item in ${master_array[*]}; do
+  echo $arr_item >>$HOST
+done
+
+echo "[kube-node]">>$HOST
+for arr_item in ${master_array[*]}; do
+  echo $arr_item >>$HOST
+done
+for arr_item in ${worker_array[*]}; do
+  echo $arr_item >>$HOST
+done
+
+echo """[k8s-cluster:children]
 kube-node
-kube-master""" > inventory/taco-aio.cfg
+kube-master""">>$HOST
 
-ansible-playbook -u root -b -i ~/apps/kubespray/inventory/taco-aio.cfg ~/apps/kubespray/cluster.yml
+echo "[controller-node]">>$HOST
+for arr_item in ${master_array[*]}; do
+  echo $arr_item >>$HOST
+done
 
-ansible-playbook -u root -b -i ~/apps/kubespray/inventory/taco-aio.cfg ~/apps/kubespray/weavescope.yml
+echo "[compute-node]">>$HOST
+for arr_item in ${worker_array[*]}; do
+  echo $arr_item >>$HOST
+done
+echo """[controller-node:vars]
+node_labels={"openstack-control-plane":"enabled", "openvswitch":"enabled"}
+[compute-node:vars]
+node_labels={"openstack-compute-node":"enabled", "openvswitch":"enabled"}""" >>$HOST
+
+ansible-playbook -u root -b -i ~/apps/upstream-kubespray/inventory/new/host.ini ~/apps/upstream-kubespray/cluster.yml
+
 
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | cat > /tmp/helm_script.sh \
-&& chmod 755 /tmp/helm_script.sh && /tmp/helm_script.sh --version v2.8.2
+&& chmod 755 /tmp/helm_script.sh && /tmp/helm_script.sh --version v2.9.1
 
-helm init --upgrade
-
-kubectl label nodes openstack-control-plane=enabled --all --namespace=openstack --overwrite
-kubectl label nodes openvswitch=enabled --all --namespace=openstack --overwrite
-kubectl label nodes openstack-compute-node=enabled --all --namespace=openstack --overwrite
-kubectl label nodes kubernetes-control-plane=enabled --all --overwrite
-kubectl label nodes ceph-mds=enabled --all --overwrite
-kubectl label nodes ceph-mon=enabled --all --overwrite
-kubectl label nodes ceph-osd=enabled --all --overwrite
-kubectl label nodes ceph-rgw=enabled --all --overwrite
-kubectl label nodes ceph-mgr=enabled --all --overwrite
-
-kubectl create clusterrolebinding openstack \
---clusterrole=cluster-admin \
---serviceaccount=openstack:default
-
-kubectl create clusterrolebinding ceph \
---clusterrole=cluster-admin \
---serviceaccount=ceph:default
-
-echo """nameserver 10.96.0.10
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-search openstack.svc.cluster.local svc.cluster.local cluster.local
-options ndots:5""" > /etc/resolv.conf
+#echo """nameserver 10.96.0.10
+#nameserver 8.8.8.8
+#nameserver 8.8.4.4
+#search openstack.svc.cluster.local svc.cluster.local cluster.local
+#options ndots:5""" > /etc/resolv.conf
 
 set -e
 
