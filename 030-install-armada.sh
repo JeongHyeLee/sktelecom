648fc490ad77c52001912dc46fc986de92492e2e
JeongHyeLee <40881134+JeongHyeLee@users.noreply.github.com>
2018-08-20 10:57:18 +0900

    Update 030-install-armada.sh
    add sudo authority

--- 030-install-armada.sh
+++ 030-install-armada.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-(( EUID )) && echo You need to be root. && exit 1
 set -ex
 
 ARMADA_DIR=~/apps/armada
@@ -11,19 +10,19 @@ cd ~/apps
 git clone http://github.com/sktelecom-oslab/armada.git && cd armada
 OS_DISTRO=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
 if [ $OS_DISTRO == Red ]; then
-    yum -y install https://rhel7.iuscommunity.org/ius-release.rpm
-    yum -y install python36u python36u-devel
-    yum install -y python36u-pip
-    pip3.6 install .
+    sudo yum -y install https://rhel7.iuscommunity.org/ius-release.rpm
+    sudo yum -y install python36u python36u-devel
+    sudo yum install -y python36u-pip
+    sudo pip3.6 install .
 elif [ $OS_DISTRO == CentOS ]; then
-    yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-    yum -y install python36u python36u-devel
-    yum install -y python36u-pip
-    pip3.6 install .
+    sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+    sudo yum -y install python36u python36u-devel
+    sudo yum install -y python36u-pip
+    sudo pip3.6 install .
 elif [ $OS_DISTRO == Ubuntu ]; then
-    apt-get install -y python3-pip
-    pip3 install --upgrade pip==9.0.3
-    pip3 install .
+    sudo apt-get install -y python3-pip
+    sudo pip3 install --upgrade pip==9.0.3
+    sudo pip3 install .
 else
     echo "This Linux Distribution is NOT supported"
 fi
